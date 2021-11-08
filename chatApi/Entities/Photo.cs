f8310e4e3b953e9441394600b29cb631390b03b6
Luis Daniel <Luis Daniel@DESKTOP-RK2RH1T>
2021-11-08 11:06:29 -0400

    add the photo entity and add the seed data of the user

--- chatApi/Entities/Photo.cs
+++ chatApi/Entities/Photo.cs
@@ -9,5 +9,8 @@ namespace chatApi.Entities
         public string Url{ get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
+
+        public AppUser AppUser { get; set; }
+        public int AppUserId { get; set; }
     }
 }
--- chatApi/Startup.cs
+++ chatApi/Startup.cs
@@ -35,11 +35,12 @@ namespace chatApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //extensions 
             services.AddApplicationServices(_config);
          
             services.AddControllers();
             services.AddCors();
-
+            //extensions 
             services.AddIdentityServices(_config);
             //add the authentication middleware
            
@@ -48,6 +49,7 @@ namespace chatApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //add the middleware errors
             app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
@@ -55,6 +57,7 @@ namespace chatApi
             app.UseRouting();
 
             app.UseAuthorization();
+            //add the cors
             app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5001"));
             //add the authentication middleware
             app.UseAuthentication();
